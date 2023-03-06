0bb716e13545223020fe00ae40f07e321aeeca1d
ChungNguyenVr912 <chungdeser@gmail.com>
2023-03-06 21:02:30 +0700

    completed survey

--- survey.js
+++ survey.js
@@ -1,7 +1,15 @@
 let f1 = document.getElementById('f3-4');
 let f2 = document.getElementById('field01');
 let f3 = document.getElementById('f3-4-1');
+let f4 = document.getElementById('genselect');
+let charcount = document.getElementById('usedchar');
+f1.addEventListener("click", check)
 f2.addEventListener("click", check)
+f4.addEventListener("click", function()
+{
+    if(f1.checked == false)
+    {f3.style.display = 'none';}
+})
 function check()
 {   
     f1.checked = true;   
@@ -11,7 +19,9 @@ f2.addEventListener("blur", function()
 {
     f3.style.display = 'none';
 })
-// function check()
-// {
-//     f1.checked = check;
-// }
+
+f2.addEventListener("input", changetext);
+function changetext()
+{
+    charcount.textContent = f2.value.length;
+}
